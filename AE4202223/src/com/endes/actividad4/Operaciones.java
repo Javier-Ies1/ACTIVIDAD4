c12f1c0de78152d49cfff7efd65240aaf4df5653
Javier <jox01008@educastur.es>
2023-04-01 17:14:39 +0200

    Versión 1

--- AE4202223/src/com/endes/actividad4/Operaciones.java
+++ AE4202223/src/com/endes/actividad4/Operaciones.java
@@ -1,27 +1,40 @@
 package com.endes.actividad4;
 
 import java.util.Scanner;
-
+/**
+ * Clase Operaciones para realizar la suma, resta, multiplicación y 
+ * división
+ * @author javier
+ *
+ */
 public class Operaciones {
-
+	/**
+	 *Método main que ejecuta el programa principal  
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
+		//Declaración de variables
 		int x,y;
 		int suma, resta, multiplicacion, division;
 		
+		//Pedir e introducir valor del primer número
 		System.out.println("Introduzca un número entero");
 		Scanner var1 = new Scanner(System.in);
 		x = var1.nextInt();
 		
+		//Pedir e introducir valor del segundo número
 		System.out.println("Introduzca otro número entero");
 		Scanner var2 = new Scanner(System.in);
 		y = var2.nextInt();
 		
+		//Realización de las operaciones
 		suma = x+y;
 		resta = x-y;
 		multiplicacion = x+y;
 		division = x/y;
 		
+		//Mostrar por pantalla los resultados
 		System.out.println("La Suma es = "+suma);
 		System.out.println("La Resta es = "+resta);
 		System.out.println("La suma es = "+multiplicacion);
